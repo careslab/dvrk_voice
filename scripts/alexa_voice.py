b24dcf8a6b9f9f5d7a6256b527672c75a4d68915
cares.lab@gmail.com <cares.lab@gmail.com>
2022-01-25 16:48:36 -0500

    add run and track to voice

--- scripts/alexa_voice.py
+++ scripts/alexa_voice.py
@@ -1,19 +1,19 @@
 import logging
-
 from flask import Flask, render_template
-
 from flask_ask import Ask, statement, question, session
+import rospy
+from std_msgs.msg import Empty
+from std_msgs.msg import Bool
+from std_msgs.msg import String
 
 
 app = Flask(__name__)
-
 ask = Ask(app, "/")
 
-logging.getLogger("flask_ask").setLevel(logging.DEBUG)
+#logging.getLogger("flask_ask").setLevel(logging.DEBUG)
 
 
 @ask.launch
-
 def new_game():
 
     welcome_msg = render_template('welcome')
@@ -21,26 +21,50 @@ def new_game():
     return question(welcome_msg)
 
 
-
-@ask.intent("RunAutoCameraIntent")
-
-def answer(run):
+@ask.intent('RunAutoCameraIntent')
+def autocamera_rub(run):
+    print("RunAutoCameraIntent: " + str(run))
     print(run)
-    print('\n')
 
-    if run == 'start':
+    if(run == True):
+        pub = rospy.Publisher('/assistant/autocamera/run', Bool, queue_size=1)
+        while pub.get_num_connections() < 1:
+            print("waiting for pub")
+        pub.publish(True)
 
-        msg = 'Starting auto camera'
-    
-    elif run == 'stop':
+    elif(run == False):
+        pub = rospy.Publisher('/assistant/autocamera/run', Bool, queue_size=1)
+        while pub.get_num_connections() < 1:
+            print("waiting for pub")
+        pub.publish(False)
 
-        msg = 'Starting auto camera'
+    say = 'Davinci has been turned on'
+    return statement(say)
 
-    else:
+@ask.intent("TrackToolIntent", convert={'tool': String})
+def autocamera_rub(tool):
+    print("RunAutoCameraIntent" + str(tool))
 
-        msg = 'Unknown variable'
+    if(tool == "Right"):
+        pub = rospy.Publisher('/assistant/autocamera/track', Bool, queue_size=1)
+        while pub.get_num_connections() < 1:
+            print("waiting for pub")
+        pub.publish("Right")
 
-    return statement(msg)
+    elif(tool == "Left"):
+        pub = rospy.Publisher('/assistant/autocamera/track', String, queue_size=1)
+        while pub.get_num_connections() < 1:
+            print("waiting for pub")
+        pub.publish("Left")
+
+    elif(tool == "Middle"):
+        pub = rospy.Publisher('/assistant/autocamera/track', String, queue_size=1)
+        while pub.get_num_connections() < 1:
+            print("waiting for pub")
+        pub.publish("Middle")
+
+    say = 'Davinci has been turned on'
+    return statement(say)
 
 
 if __name__ == '__main__':
